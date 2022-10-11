2096ffbe974707b93c1ea67f1c67f579a088bf7e
Mohammad Asif Siddique <mdasifsidd@gmail.com>
2022-10-11 16:31:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+import Home from './Home'
+function App() {
+   
+  return (
+    <>
+    <Modal/>
+    <Sidebar/>
+    <Home/>
+    </>
+  )
+}
+
+export default App
